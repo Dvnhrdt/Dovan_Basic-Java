1230651044f11937b5543c90f44a383dcba3bac7
unknown <harditiadovan@gmail.com>
2021-10-12 19:27:04 +0700

    first commit

---
+++ Biodataku.java
@@ -0,0 +1,21 @@
+public class Biodataku {
+    public static void main(String args []) {
+        String firstName = "Ridwan ";
+        String lastName = "Syarif";
+        String address = "Bukittinggi";
+        int age = 20;
+        String tahun = " Tahun";
+        String phoneNumber = "082372638492";
+        Boolean single = true;
+        String hobi = "Futsal";
+        String makananFav = "Bakso";
+        System.out.println("First name   : " + firstName + lastName);
+        System.out.println("Address      : " + address);
+        System.out.println("Age          : " + age + tahun);
+        System.out.println("Phone Number : " + phoneNumber);
+        System.out.println("Hobi         : " + hobi);
+        System.out.println("Single       : " + single);
+        System.out.println("Makanan Fav  : " + makananFav);
+
+    }
+}
