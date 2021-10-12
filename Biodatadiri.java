3ae6e1c94c4d7ff8ccc83b7dd579efe22bda0daf
unknown <harditiadovan@gmail.com>
2021-10-12 18:56:37 +0700

    first commit

---
+++ Biodatadiri.java
@@ -0,0 +1,17 @@
+public class Biodatadiri {
+    public static void main(String args []) {
+        String firstName = "Dovan ";
+        String lastName = "Harditia";
+        String address = "Bukittinggi";
+        int age = 20;
+        String tahun = " Tahun";
+        String phoneNumber = "081122334455";
+        String hobi = "Sepak Bola";
+        System.out.println("First name   : " + firstName + lastName);
+        System.out.println("Address      : " + address);
+        System.out.println("Age          : " + age + tahun);
+        System.out.println("Phone Number : " + phoneNumber);
+        System.out.println("Hobi         : " + hobi);
+
+    }
+}
