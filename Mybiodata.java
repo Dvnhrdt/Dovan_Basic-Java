0e9c54ca2b9ac87f1398c3128b78423d8b5dcb31
unknown <harditiadovan@gmail.com>
2021-10-12 19:16:49 +0700

    first commit

---
+++ Mybiodata.java
@@ -0,0 +1,19 @@
+public class Mybiodata{
+    public static void main(String args []) {
+        String firstName = "Muhammad ";
+        String lastName = "Fauzi";
+        String address = "Bukittinggi";
+        int age = 20;
+        String tahun = " Tahun";
+        String phoneNumber = "081214151617";
+        String hobi = "Basket Ball";
+        String makananFav = "Spaghetti";
+        System.out.println("First name   : " + firstName + lastName);
+        System.out.println("Address      : " + address);
+        System.out.println("Age          : " + age + tahun);
+        System.out.println("Phone Number : " + phoneNumber);
+        System.out.println("Hobi         : " + hobi);
+        System.out.println("Makanan Fav  : " + makananFav);
+
+    }
+}
